7707af8edc2caaa8b49fcc863d51d72f1161116c
Sharad Bhat <sharad.mbhat@gmail.com>
2024-05-10 02:53:56 -0700

    added prettier

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
-import './App.css'
+import './App.css';
 
 function App() {
   return (
     <>
       <h2>Hi, I'm</h2>
       <h1>💕Zoey Kourianos💕</h1>
+      <h3>I'm a UI/UX Designer</h3>
+      <h5>P.S. I know this isn't amazing right now. My boyfriend made it.</h5>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
